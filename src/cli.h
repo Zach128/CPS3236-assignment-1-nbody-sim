542e14c8fac95be9e16d7858c420ddc70b5ec73d
Zachary Cauchi <zach.cauchi@hotmail.com>
2021-01-04 22:47:46 +0100

    feat: add cli module

---
+++ src/cli.h
@@ -0,0 +1,19 @@
+#ifndef CLI_H_
+#define CLI_H_
+
+#include <stdbool.h>
+
+struct _nbody_options {
+    char *file_path;
+    bool output;
+    int num_particles;
+    float grav_constant;
+    int num_iterations;
+    float time_step;
+};
+
+typedef struct _nbody_options cli_opt;
+
+struct argparse get_argparse();
+
+#endif
