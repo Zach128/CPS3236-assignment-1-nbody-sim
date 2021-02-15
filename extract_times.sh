815778d63f77e0e50122b0c80141f6f82a73dbce
Zachary Cauchi <zach.cauchi@hotmail.com>
2021-02-15 21:29:55 +0100

    feat: add ignoring of bad results

--- extract_times.sh
+++ extract_times.sh
@@ -3,8 +3,14 @@ files=($(find $1*))
 avg=0
 for o in "${files[@]}"
 do
-    buf=$(grep -Eo '[0-9.]+ seconds' $o | tail -1 | sed -e "s/\ seconds//")
-    avg=$(echo "$buf + $avg" | bc -l)
+  buf=$(grep -Eo '[0-9.]+ seconds' $o | tail -1 | sed -e "s/\ seconds//")
+  buf=${buf:="-1"}
+  
+  if [ $buf != "-1" ]
+  then
+    echo $o: $buf
+    avg=$(echo "$buf + $avg" | bc -l)        
+  fi
 done
 
 avg=$(echo "$avg / ${#files[@]}" | bc -l)
